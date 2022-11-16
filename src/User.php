d369ff308096a0c12a5e6a0ecd28ae9fb068e319
ai2k2 <95246271+ai2k2@users.noreply.github.com>
2022-11-16 12:40:34 +0000

    bump to 1.2.1

--- src/User.php
+++ src/User.php
@@ -6,3 +6,7 @@ final class User {
   public function __construct(
     private string $name,
     private int $id,
+    private Vector<int> $friend_ids,
+  ) {}
+
+  public function getName(): string {
