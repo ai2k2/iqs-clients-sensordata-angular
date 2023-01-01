313e619fa2d468bac1f9d7ae4fe9a8fb502d4800
ai2k2 <95246271+ai2k2@users.noreply.github.com>
2023-01-01 08:32:30 +0000

    refactoring in progress

--- src/__tests__/EmailTestCase.php
+++ src/__tests__/EmailTestCase.php
@@ -6,3 +6,5 @@ use PHPUnit\Framework\TestCase;
 final class EmailTestCase extends TestCase {
 
   public function testCanBeCreatedFromValidEmailAddress() {
+    $this->assertInstanceOf(
+      Email::class,
